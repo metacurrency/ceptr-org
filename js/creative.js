06817ec95eb9a69438caffd5ac0d6a0c93877754
Eric Harris-Braun <eric@harris-braun.com>
2017-01-24 16:49:14 -0500

    standardizing file indents

--- js/creative.js
+++ js/creative.js
@@ -20,7 +20,7 @@
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -57,7 +57,7 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Move toc
     var toc = document.getElementById('toc');
